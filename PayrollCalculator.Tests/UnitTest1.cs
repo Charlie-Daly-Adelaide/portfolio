ac0d473bb64a119925797c0da580db73ef9f90bd
Charlie Daly <dalcl002@mymail.unisa.edu.au>
2026-03-12 09:26:40 +1030

    feat: payroll class

---
+++ PayrollCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace PayrollCalculator.Tests;
+using Xunit;
+using PayrollCalculator;
+
+public class payrollTest
+{
+    [Fact]
+    public void CorrectTaxCalcTest()
+    {
+        var p = new PayrollCalculator(3, 5, 2);
+        Assert.Equal(-15, p.CalculateNetPay());
+    }
+    public void CorrectTaxChangeTest()
+    {
+        var p = new PayrollCalculator(3, 5, 2);
+        p.ChangeTaxRate(1);
+        Assert.Equal(1, p.GetTaxRate());
+    }
+}
